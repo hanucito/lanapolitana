4b3fd6f0058318be66d438b8555cb634e416aaf6
Hanu <hanuman.dasa@gmail.com>
2020-06-20 12:36:55 -0300

    initial

---
+++ docs/precache-manifest.723604afb4fb2ca8772b763dbdc14d0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc7095594546aab57af7815547f575f",
+    "url": "./docs/index.html"
+  },
+  {
+    "revision": "f5576354e8d4d258d07e",
+    "url": "./docs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dcf493fa82186e8e741",
+    "url": "./docs/static/js/2.40334054.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./docs/static/js/2.40334054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5576354e8d4d258d07e",
+    "url": "./docs/static/js/main.0f7ac0ae.chunk.js"
+  },
+  {
+    "revision": "00d25a2a5d40fce787c6",
+    "url": "./docs/static/js/runtime-main.a82f9885.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./docs/static/media/logo.5d5d9eef.svg"
+  }
+]);
